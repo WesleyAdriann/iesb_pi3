a28ee89fe76c57420b00c7fe7987886c1ec0971d
Wesley Adriann <wesleyadriann@gmail.com>
2020-10-19 20:53:23 -0300

    Adds main function and mutation
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- code/CreateGroup.py
+++ code/CreateGroup.py
@@ -1,23 +1,31 @@
 
 # -*- coding: utf-8 -*-
 
-from random import randint
+from random import randint, random
 
 class CreateGroup():
-    def __init__(self, ngroups, students_lim, **kwa):
+    def __init__(self, ngroups, students_per_group, **kwargs):
         self.__ngroups = ngroups
-        self.__students_lim = students_lim
+        self.__students_lim = []
         self.__groups = []
         self.__best_group = []
         self.__fitness = []
+        self.__students_per_group = students_per_group
+        self.__rate_mutation = kwargs.get('rate_mutation', 0.5)
 
     @property
     def groups(self):
         return self.__groups
 
-    @property
-    def best_group(self):
-        return self.__best_group
+    def main(self, ngenerations = 30):
+        # students_lim = [[0, 255]]*student_per_group
+        self.__students_lim = [[1, 90], [10, 90], [0, 23]]
+        self.create_group()
+        for i in range(ngenerations):
+            print('gens')
+            # pass
+        print('Best group')
+        print(', '.join(self.__best_group))
 
     def create_group(self):
         nstudents = len(self.__students_lim)
@@ -25,7 +33,7 @@ class CreateGroup():
             student = []
             for j in range(nstudents):
                 lim_inf, lim_sup = self.__students_lim[j]
-                student.append(randint(0, 1001) * (lim_sup-lim_inf) + lim_inf)
+                student.append(randint(0, 1000) * (lim_sup-lim_inf) + lim_inf)
             self.__groups.append(student)
 
     def fitness(self):
@@ -47,4 +55,10 @@ class CreateGroup():
         pass
 
     def mutate(self):
-        pass
+        groups_mutate = self.__groups[:]
+        for i, group in enumerate(groups_mutate):
+            if(random() < self.__rate_mutation):
+                mutation_point = randint(0, self.__students_per_group)
+                groups_mutate[i][mutation_point] = randint(0, 1000)
+        self.__groups = groups_mutate
+
--- code/main.py
+++ code/main.py
@@ -6,8 +6,6 @@ from CreateGroup import CreateGroup
 if __name__ == "__main__":
     ngroups = 5
     student_per_group = 3
-    # students_lim = [[0, 255]]*student_per_group
-    students_lim = [[1, 90], [10, 90], [0, 23]]
     group = CreateGroup(ngroups, students_lim)
     group.create_group()
     print(group.groups)
