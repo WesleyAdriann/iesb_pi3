b39364f16e9b9e5f650994ad18c264768b138b85
Wesley Adriann <wesleyadriann@gmail.com>
2020-10-19 20:05:40 -0300

    Adds fitness method
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- code/CreateGroup.py
+++ code/CreateGroup.py
@@ -4,14 +4,20 @@
 from random import randint
 
 class CreateGroup():
-    def __init__(self, ngroups, students_lim):
+    def __init__(self, ngroups, students_lim, **kwa):
         self.__ngroups = ngroups
         self.__students_lim = students_lim
         self.__groups = []
+        self.__best_group = []
+        self.__fitness = []
 
     @property
     def groups(self):
         return self.__groups
+
+    @property
+    def best_group(self):
+        return self.__best_group
 
     def create_group(self):
         nstudents = len(self.__students_lim)
@@ -23,6 +29,18 @@ class CreateGroup():
             self.__groups.append(student)
 
     def fitness(self):
+        for group in self.groups:
+            self.fitness.append(self.fitness_fn(group))
+
+
+    def fitness_fn(self, group):
+        fit = 0
+        for student in group:
+            fit = fit + student
+        return fit
+
+
+    def selection(self):
         pass
 
     def crossing(self):
