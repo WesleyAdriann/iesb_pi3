cf70ac38b3be6e2811f2081df895ef528caa8591
Wesley Adriann <wesleyadriann@gmail.com>
2020-11-07 18:40:14 -0300

    Changes subjects to list
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>
    Co-authored-by: Bruno Mota  <bruno.teomota@gmail.com>
    Co-authored-by: Lucas Simplicio  <lucas.snk.2013@hotmail.com>

--- code/models/Student.py
+++ code/models/Student.py
@@ -1,19 +1,19 @@
 
 # -*- coding: utf-8 -*-
 
-from random import uniform, randrange
+from random import uniform, randint
 
 from models.Subject import Subject
 
 class Student():
     def __init__(self, amount_subjects = 3):
         self.__subjects = [Subject()]*amount_subjects
-        self.__matematica = Subject()
-        self.__portugues = Subject()
         self.__average = 0
         self.calc_average()
 
     def __repr__(self):
+        # subjects_notes = ", ".join([subject.note for subject in self.__subjects])
+        # return "notas: {subject_notes} - media: {:.2f}".format(subjects_notes, self.__average)
         return "{:.2f}".format(self.__average)
 
     @property
@@ -21,11 +21,9 @@ class Student():
         return self.__average
 
     def random_note(self):
-        note = uniform(0, 11)
-        subjects = (self.__matematica,self.__portugues)
-        subjects[randrange(0, len(subjects), 1)].set_note(note)
+        self.__subjects[randint(0, len(self.__subjects) - 1)].set_random_note()
         self.calc_average()
 
     def calc_average(self):
-        subjects_notes = (self.__matematica.note, self.__portugues.note)
+        subjects_notes = [subject.note for subject in self.__subjects]
         self.__average = sum(subjects_notes) / len(subjects_notes)
