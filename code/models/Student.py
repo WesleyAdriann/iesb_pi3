79b0e2578477141afb58f1d4dc7443a51c0b8f7e
Wesley Adriann <wesleyadriann@gmail.com>
2020-11-16 22:44:03 -0300

    Adds to read file with subject names and adds student age and favorite subject
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>
    Co-authored-by: Bruno Mota  <bruno.teomota@gmail.com>
    Co-authored-by: Lucas Simplicio  <lucas.snk.2013@hotmail.com>

--- code/models/Student.py
+++ code/models/Student.py
@@ -1,13 +1,21 @@
 
 # -*- coding: utf-8 -*-
 
-from random import uniform, randrange
+from random import uniform, randint
+from os import path
 
 from models.Subject import Subject
+from utils.read_file import read_file
 
 class Student():
-    def __init__(self, amount_subjects = 3):
-        self.__subjects = [Subject()]*amount_subjects
+    __subjects_list = []
+    __subjects_list = __subjects_list if len(__subjects_list) else read_file(path.abspath('subject_names.txt'))
+
+    def __init__(self):
+        print('Creating Student')
+        self.__subjects = [Subject(subject) for subject in self.__subjects_list]
+        self.__favorite_subject = self.__subjects_list[randint(0, len(self.__subjects_list) - 1)]
+        self.__age = randint(18, 60)
         self.__average = 0
         self.calc_average()
 
